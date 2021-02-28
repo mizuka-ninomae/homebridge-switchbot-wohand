bdb40327b2482f411ac989ebff1efea9ce8af5ae
mizuka-ninoae <mizuka_ninomae@icloud.com>
2021-02-28 16:23:25 +0900

    ソースコードを整理

--- index.js
+++ index.js
@@ -1,127 +1,113 @@
 let Service, Characteristic;
-const execa      = require('execa');
+const execa         = require ('execa');
+const cron          = require ('node-cron');
 
-module.exports = function(homebridge) {
-  Service        = homebridge.hap.Service;
-  Characteristic = homebridge.hap.Characteristic;
-  homebridge.registerAccessory('homebridge-switchbot-wohand', 'switchbot-wohand', switchbotWHAccessory);
+module.exports = function (homebridge) {
+  Service           = homebridge.hap.Service;
+  Characteristic    = homebridge.hap.Characteristic;
+  homebridge.registerAccessory ('homebridge-switchbot-wohand', 'switchbot-wohand', switchbotWHAccessory);
 }
 
-//------------------------------------------------------------------------------
-function switchbotWHAccessory(log, config) {
+function switchbotWHAccessory (log, config) {
   this.log          = log;
-  this.name         = config['name'];
-  this.BleMac       = config['BleMac'];
-  this.onCmd        = config['on'] || 570101;
-  this.offCmd       = config['off'] || 570102;
-  if ((this.onCmd == 570101 && this.offCmd == 570102) ||
-      (this.onCmd == 570104 && this.offCmd == 570103) ||
-      (this.onCmd == 570100 && this.offCmd == 570100)) {
+  this.name         = config ['name'];
+  this.BleMac       = config ['BleMac'];
+  this.onCmd        = config ['on'] || 570101;
+  this.offCmd       = config ['off'] || 570102;
+  if ((this.onCmd  == 570101 && this.offCmd == 570102) ||
+      (this.onCmd  == 570104 && this.offCmd == 570103) ||
+      (this.onCmd  == 570100 && this.offCmd == 570100)) {
   }
   else {
     this.onCmd      = 570101;
     this.offCmd     = 570102;
   }
-  this.button_off   = config['button_off'] || 8000;
-  if (this.button_off < 5000) { this.button_off = 5000; }
-  this.dummy_log    = config['dummy_log'] || false;
-  this.retry        = config['retry'] || 5;
-  if (this.retry < 1) { this.retry = 1; }
-  this.onCommand    = 'gatttool -t random -b ' + this.BleMac + ' --char-write-req -a 0x0016 -n ' + this.onCmd;
-  this.offCommand   = 'gatttool -t random -b ' + this.BleMac + ' --char-write-req -a 0x0016 -n ' + this.offCmd;
-  this.stateCommand = 'gatttool -t random -b ' + this.BleMac + ' --char-read -a 0x0016';
-  this.onValue      = 'Characteristic value/descriptor: ' + this.onCmd + ' ';
-  this.onValue      = this.onValue.toLowerCase().replace(/\s+/g, "");
-  if (this.onCmd === this.offCmd) {
-    this.type = 'button';
-  }
-  else {
-    this.type = 'switch';
-  }
-  this.informationService = new Service.AccessoryInformation();
-  this.switchService      = new Service.Switch(this.name);
+  this.schedule     = config ["schedule"] || '*/10 * * * * *';
+  this.dummy_log    = config ['dummy_log'] || false;
+  this.retry        = (this.retry < 1) ? 1 : config ['retry'] || 5;
+  this.onCommand    = `gatttool -t random -b ${this.BleMac} --char-write-req -a 0x0016 -n ${this.onCmd}`;
+  this.offCommand   = `gatttool -t random -b ${this.BleMac} --char-write-req -a 0x0016 -n ${this.offCmd}`;
+  this.stateCommand = `gatttool -t random -b ${this.BleMac} --char-read -a 0x0016`
+  this.onValue      = `Characteristic value/descriptor: ${this.onCmd} `;
+  this.onValue      = this.onValue.toLowerCase ().replace (/\s+/g, "");
+  this.type         = (this.onCmd === this.offCmd) ? "button" : "switch" ;
 
-  this.informationService
-  .setCharacteristic(Characteristic.Manufacturer, 'switchbot-wohand Manufacturer')
-  .setCharacteristic(Characteristic.Model, 'switchbot-wohand Model')
-  .setCharacteristic(Characteristic.SerialNumber, 'switchbot-wohand Serial Number');
-
-  this.switchService
-  .getCharacteristic(Characteristic.On)
-  .on('set', this.setState.bind(this))
-
-  this.switchService
-  .getCharacteristic(Characteristic.On)
-  .on('get', this.getState.bind(this));
+  this.informationService = new Service.AccessoryInformation ();
+  this.switchService      = new Service.Switch (this.name);
 
   if (this.type == 'button') {
-    this.switchService.setCharacteristic(Characteristic.On, false);
+    let job = cron.schedule(this.schedule, () => {
+      this.switchService.updateCharacteristic (Characteristic.On, false);
+      if (this.dummy_log) {this.log (` <<<< [SET Button(Dummy)：OFF]`); }
+    })
   }
 }
 
-//------------------------------------------------------------------------------
-switchbotWHAccessory.prototype.getServices = function() {
+switchbotWHAccessory.prototype.getServices = function () {
+  this.informationService
+    .setCharacteristic (Characteristic.Manufacturer, 'switchbot-wohand Manufacturer')
+    .setCharacteristic (Characteristic.Model, 'switchbot-wohand Model')
+    .setCharacteristic (Characteristic.SerialNumber, 'switchbot-wohand Serial Number');
+
+  this.switchService
+    .getCharacteristic (Characteristic.On)
+    .on ('set', this.setState.bind (this))
+    .on ('get', this.getState.bind (this));
+
   return [this.informationService, this.switchService];
 }
 
-//------------------------------------------------------------------------------
-switchbotWHAccessory.prototype.setState = function(powerOn, callback) {
-  if      (this.type == 'button' && powerOn) {
+switchbotWHAccessory.prototype.setState = function (powerOn, callback) {
+  if      (this.type == "button" && powerOn) {
     for (let j=1; j<=this.retry; j++) {
       try {
-        let SState = execa.commandSync(this.onCommand);
-        this.log(' <<<< [SET Button： ' + powerOn + ']');
-        callback(null);
+        execa.commandSync (this.onCommand);
+        this.log (` <<<< [SET Button： ${powerOn ? "ON" : "OFF"}]`);
+        callback (null);
         return;
       }
       catch {
-        setTimeout(function(){}, 50000);
-        this.log('SET Button Retry: ' + j);
-        continue;
+        this.log (`SET Button Retry: ${j}`);
+        setTimeout (function () {}, 50000);
       }
     }
+    this.switchService
+      .updateCharacteristic (Characteristic.On, new Error(`Error: ${this.name}`));
   }
 
-  else if (this.type == 'button' && !powerOn) {
-    setTimeout(function() {
-      this.switchService.setCharacteristic(Characteristic.On, false);
-    }.bind(this), this.button_off);
-    if (this.dummy_log) {this.log(' <<<< [SET Button(Dummy)： ' + powerOn + ']'); }
-    callback(null);
-    return;
-  }
-
-
-  else if (this.type == 'switch') {
+  else if (this.type == "switch") {
     for (let j=1; j<=this.retry; j++) {
       try {
-        let SState = execa.commandSync(powerOn ? this.onCommand : this.offCommand);
-        this.log(' <<<< [SET Switch： ' + powerOn + ']');
-        callback(null);
+        execa.commandSync ((powerOn ? this.onCommand : this.offCommand));
+        this.log (` <<<< [SET Switch： ${powerOn ? "ON" : "OFF"}]`);
+        callback (null);
         return;
       }
       catch {
-        setTimeout(function(){}, 50000);
-        this.log('SET Switch Retry: ' + j);
-        continue;
+        this.log (`SET Switch Retry: ${j}`);
+        setTimeout (function () {}, 50000);
       }
     }
+    this.switchService
+      .updateCharacteristic (Characteristic.On, new Error(`Error: ${this.name}`));
   }
 }
-//------------------------------------------------------------------------------
-switchbotWHAccessory.prototype.getState = function(callback) {
+
+switchbotWHAccessory.prototype.getState = function (callback) {
   for (let i=1; i<=this.retry; i++) {
     try {
-      let GState = execa.commandSync(this.stateCommand);
-      let cleanOut = GState.stdout.toLowerCase().replace(/\s+/g, "");
-      this.log(' >>>> [Status GET]');
-      callback(null, (this.type == 'button') ? false : cleanOut == this.onValue);
-      return;
+      let gState = execa.commandSync (this.stateCommand);
+        let cleanOut = gState.stdout.toLowerCase ().replace (/\s+/g, "");
+        this.log (` >>>> [Status GET]`);
+        callback (null, (this.type == 'button') ? false : cleanOut == this.onValue);
+        return;
       }
     catch {
-      setTimeout(function(){}, 100000);
-      this.log('GET Retry: ' + i);
-      continue;
+      this.log (`GET Retry: ${i}`);
+      setTimeout (function () {}, 100000);
+
     }
   }
+  this.switchService
+    .updateCharacteristic (Characteristic.On, new Error(`Error: ${this.name}`));
 }
